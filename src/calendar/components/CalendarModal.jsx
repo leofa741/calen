bcc213b8cefa691f62f52e3cc7797b93c537845d
leofa741 <61668570+leofa741@users.noreply.github.com>
2022-08-12 16:41:03 -0300

    modal-ok

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -1,7 +1,18 @@
 
 import Modal from 'react-modal'
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import './Modal.css'
+import Button from '@mui/material/Button';
+import CloseIcon  from '@mui/icons-material/Close';
+import { addHours } from 'date-fns';
+import DatePicker, {registerLocale}from "react-datepicker";
+import{es} from "date-fns/locale";
+import "react-datepicker/dist/react-datepicker.css";
+import Swal from 'sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
+
+
+registerLocale('es', es);
 
 const customStyles = {
     content: {
@@ -19,30 +30,157 @@ const customStyles = {
 export const CalendarModal = () => {
 
 
-
     const [isOpen, setIsOpen] = useState(true);
-    const closeModal = () => {
 
+    const [formValues, setFormValues] = useState({
+        title: 'leo',
+        notes: 'la nota',
+        start: new Date(),
+        end :addHours(new Date(), 1),
+    })
+    const [submitted, setSubmitted] = useState(false);
+
+    const titleClass = useMemo(() => {   
+        return formValues.title.length  > 0  ? 'is-valid' : 'is-invalid';   
+    } , [ formValues.title]);
+
+
+    const notesClass = useMemo(() => {  
+      return formValues.notes.length  > 0  ? 'is-valid' : 'is-invalid';  
+  } , [ formValues.notes]);
+ 
+    
+    const handleChange = (e) => {
+        setFormValues({
+            ...formValues,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleDateChange = (e,changing) => {
+        setFormValues({
+            ...formValues,
+            [changing]: e
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setSubmitted(true);
+
+        const difference = formValues.end.getTime() - formValues.start.getTime();
+        const hours = Math.ceil(difference / (1000 * 60 * 60));
+
+        if (hours < 0) {            
+            Swal.fire('Error', 'La fecha de inicio no puede ser mayor a la fecha de fin', 'error')        
+
+         console.log(hours)
+           return
+             } 
+
+           if(formValues.title === '' || formValues.notes === ''){
+            Swal.fire('Error', 'Todos los campos son obligatorios', 'error')
+             console.log('error')
+               return
+                }
+
+                if(submitted){
+                    Swal.fire('Exito', 'Evento creado con exito', 'success')
+                    console.log('exito')
+                }           
+                          
+          console.log(hours)
+            console.log(formValues);
+          }
+  
+
+    const closeModal = () => {
         setIsOpen(false)
     }
 
   return (
     <Modal
-        isOpen={isOpen}     
-   
+        isOpen={isOpen}        
         onRequestClose={closeModal}
         style={customStyles}
         closeTimeoutMS={200}
         className="modal"
-        overlayClassName="modal-fondo"
-       
-
+        overlayClassName="modal-fondo"  
     >
-<h1>hola modal</h1>
-<hr></hr>
-<button onClick={closeModal}>close</button>
+        
+    
+<Button variant="contained" color="error" className='Button'   onClick={closeModal} endIcon={<CloseIcon />}>Close
+</Button>
 
 
+<h1> Nuevo evento </h1>
+<hr />
+<form className="container"  onSubmit={ handleSubmit } >
+    <div className="form-group mb-2">
+        <label>Fecha y hora inicio</label>
+        <DatePicker
+            selected={formValues.start}
+            onChange={(e) => handleDateChange(e, 'start')}
+            name="start"
+            className="form-control"
+            dateFormat="dd/MM/yyyy"
+            locale="es"          
+            placeholderText="Seleccione una fecha y hora"         
+            required
+        />
+    </div>
+
+    <div className="form-group mb-2">
+        <label>Fecha y hora fin</label>
+        <DatePicker
+            selected={formValues.end}
+            minDate={formValues.start}
+            onChange={(e) => handleDateChange(e, 'end')}
+            name="end"
+            className="form-control"
+            dateFormat="dd/MM/yyyy"
+            locale="es"
+            placeholderText="Seleccione una fecha y hora"
+            required
+        />
+    </div>
+
+    <hr />
+    <div className="form-group mb-2">
+        <label>Titulo y notas</label>
+        <input 
+            type="text" 
+            className={`form-control ${titleClass}`}
+            placeholder="Título del evento"
+            name="title"
+            autoComplete="off"
+            value={formValues.title}
+            onChange={handleChange}
+        />
+        <small id="emailHelp" className="form-text text-muted">Una descripción corta</small>
+    </div>
+
+    <div className="form-group mb-2">
+        <textarea 
+            type="text" 
+            className={`form-control ${notesClass}`}
+            placeholder="Notas"
+            rows="5"
+            name="notes"
+            value={formValues.notes}  
+            onChange={handleChange}
+        ></textarea>
+        <small id="emailHelp" className="form-text text-muted">Información adicional</small>
+    </div>
+
+    <button
+        type="submit"
+        className="btn btn-outline-primary btn-block"
+    >
+        <i className="far fa-save"></i>
+        <span> Guardar</span>
+    </button>
+</form>
 
     </Modal>
   )
--- src/calendar/components/NavBar.jsx
+++ src/calendar/components/NavBar.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+import LogoutIcon from '@mui/icons-material/Logout'
+
 
 
 export const NavBar = () => {
@@ -10,12 +14,18 @@ export const NavBar = () => {
         &nbsp;My Calendar
         </span>
 
-<button className="btn btn-primary" onClick={() => {}}>
+{/* <button className="btn btn-primary" onClick={() => {}}>
 
     <i className="fas fa-sign-out-alt" aria-hidden="true"></i>
     &nbsp;Logout
 
-    </button>
+    </button> */}
+
+
+<Button variant="contained" endIcon={<LogoutIcon />}>
+ Logout
+</Button>
+   
 
     </div>
 
